0334907fecae0a016d976f770e48e62d4bbc9bf6
Rabin Rai <rabinrai44@gmail.com>
2018-01-21 02:16:34 -0500

    Deleted test component and added Card component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 //Service workers as a new feature that allows our apps to become faster and potentially work ofline.
 import registerServiceWorker from './registerServiceWorker';
 //tachyon similar to bootstrap pre-defined class that create element a lot faster
 import 'tachyons';
 
-ReactDOM.render(<Hello greeting={ 'Hello' + 'React Ninja'}/>, document.getElementById('root'));
+ReactDOM.render(
+        <div>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    , document.getElementById('root'));
 registerServiceWorker();
