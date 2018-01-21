843b1cbed1309041682997295d965ab155461411
Rabin Rai <rabinrai44@gmail.com>
2018-01-20 21:32:31 -0500

    Created Hello World

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
 registerServiceWorker();
