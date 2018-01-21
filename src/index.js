c13cf57d522b2120716e343ab8283a2887541cd9
Rabin Rai <rabinrai44@gmail.com>
2018-01-20 23:38:04 -0500

    Created React first component as Hello.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
+//Service workers as a new feature that allows our apps to become faster and potentially work ofline.
 import registerServiceWorker from './registerServiceWorker';
+//tachyon similar to bootstrap pre-defined class that create element a lot faster
+import 'tachyons';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={ 'Hello' + 'React Ninja'}/>, document.getElementById('root'));
 registerServiceWorker();
