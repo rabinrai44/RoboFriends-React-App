2354296adb4ab508d32181c5ea01ac833579e962
Rabin Rai <rabinrai44@gmail.com>
2018-01-21 02:56:28 -0500

    Added robots pre-define list and render data

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
 //tachyon similar to bootstrap pre-defined class that create element a lot faster
 import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
         <div>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+            <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+            <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+            <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
         </div>
     , document.getElementById('root'));
 registerServiceWorker();
