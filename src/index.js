641a911de58235c61460e5d5d860bf46c9fbb85f
Rabin Rai <rabinrai44@gmail.com>
2018-01-21 22:30:05 -0500

    Added title page, search field and search logic for find robots

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 //Service workers as a new feature that allows our apps to become faster and potentially work ofline.
 import registerServiceWorker from './registerServiceWorker';
 //tachyon similar to bootstrap pre-defined class that create element a lot faster
 import 'tachyons';
-import { robots } from './robots';
 
-ReactDOM.render(
-        <CardList robots={robots} />
-    , document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
