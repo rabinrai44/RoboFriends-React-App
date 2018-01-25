832260ea16a4ee799dd1e7a60829a69b860c834f
Rabin Rai <rabinrai44@gmail.com>
2018-01-25 02:47:03 -0500

    Update application with dynamic data from server and fixed some error

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
     const { name, email, id } = props;
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='photo' src={`https://robohash.org/${id}?200x200`}  />
+            <img alt='this is photos tag' src={`https://robohash.org/${id}?200x200`}  />
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
