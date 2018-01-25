2fc51f95351a274f3c4848f34ed3fd06b18b3fda
Rabin Rai <rabinrai44@gmail.com>
2018-01-25 14:30:24 -0500

    Bootstrap added for front-end

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './containers/App';
 //Service workers as a new feature that allows our apps to become faster and potentially work ofline.
 import registerServiceWorker from './registerServiceWorker';
 //tachyon similar to bootstrap pre-defined class that create element a lot faster
-import 'tachyons';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
