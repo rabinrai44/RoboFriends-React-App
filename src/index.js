0267812baeaa0ace9c1a00470d20163119a0d6a0
Rabin Rai <rabinrai44@gmail.com>
2018-01-25 03:27:39 -0500

    Refactor files and clean-up code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 //Service workers as a new feature that allows our apps to become faster and potentially work ofline.
 import registerServiceWorker from './registerServiceWorker';
 //tachyon similar to bootstrap pre-defined class that create element a lot faster
